08dff21c9b6d9bcdb6f0c5ac532f13379360322e
yb6436 <31675690+yb6436@users.noreply.github.com>
2017-10-17 19:52:31 +0900

    C_6st_assignment
    C_6st_assignment
    아스키코드

---
+++ C_6st_assignment.c
@@ -0,0 +1,57 @@
+ #include <stdio.h>
+
+int main()
+{
+	char code1=49;
+	char code2 = 50;
+	char code3 = 51;
+		char code4 = 52;
+		char code5 = 53;
+		char code6 = 54;
+		char code7 = 55;
+		char code8 = 56;
+		char code9 = 57;
+	int i;
+
+	for (i = 0; i < 1; i++)
+	{
+		printf("%c\n", code1);
+	}
+
+	for (i = 0; i < 2; i++)
+	{
+		printf("%c\n", code2);
+	}
+	for (i = 0; i < 3; i++)
+	{
+		printf("%c\n", code3);
+	}
+	for (i = 0; i < 4; i++)
+	{
+		printf("%c\n", code4);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%c\n", code5);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		printf("%c\n", code6);
+	}
+	for (i = 0; i < 7; i++)
+	{
+		printf("%c\n", code7);
+	}
+	for (i = 0; i < 8; i++)
+	{
+		printf("%c\n", code8);
+	}
+	for (i = 0; i < 9; i++)
+	{
+		printf("%c\n", code9);
+	}
+
+
+
+	return 0;
+}
